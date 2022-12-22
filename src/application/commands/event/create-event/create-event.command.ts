8379ea31d31ffc127ad0bfb8297057878496ffa4
Ognjen Atlagic <ognjen.atlagic@un1quely.com>
2022-12-22 16:19:16 +0100

    Implemented create event

--- src/application/commands/event/create-event/create-event.command.ts
+++ src/application/commands/event/create-event/create-event.command.ts
@@ -1,4 +1,4 @@
-import { CreateEventDto } from 'src/application/dto/event/create-event.do';
+import { CreateEventDto } from 'src/application/dto/event/create-event.dto';
 
 export class CreateEventCommand {
   constructor(public readonly dto: CreateEventDto) {}
